08519c818b0243184d9dd919ba88caf33984ea57
aykaton <30314524+aykaton@users.noreply.github.com>
2025-03-11 17:57:17 +0100

    Initial

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import "../styles/index.scss";
+// import throttle from "lodash.throttle";
+// import Swiper from "swiper";
+// import { Autoplay } from "swiper/modules";
+
+// import { isMobile } from "./utils.js";
+// import initDisclosures from "./disclosure.js";
+// import Lenis from 'lenis';
