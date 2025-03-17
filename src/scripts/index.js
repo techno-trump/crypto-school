da923e6da07ca685d734929dd3e7a2e9dfb1871a
aykaton <30314524+aykaton@users.noreply.github.com>
2025-03-17 15:17:48 +0100

    Theme switch completed. Header adaptation. Fixes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import "../styles/index.scss";
 // import throttle from "lodash.throttle";
 // import Swiper from "swiper";
 // import { Autoplay } from "swiper/modules";
+import { initPortals } from "./portal.js";
 
 import { isMobile } from "./utils.js";
 // import initDisclosures from "./disclosure.js";
@@ -16,3 +17,24 @@ document.documentElement.classList.toggle("is-mobile", isMobile.any());
 
 window.app.drawers.init();
 document.documentElement.classList.add("is-loaded");
+window.app.drawers.get("cabinet-menu").setOptions({ lockPageScroll: false });
+window.app.drawers.get("main-menu").setOptions({ lockPageScroll: false });
+
+initThemes();
+initPortals();
+
+function initThemes() {
+	const elems = document.querySelectorAll(".theme-switch__switch");
+
+	const currentTheme = localStorage.getItem("theme") || "light";
+	document.documentElement.setAttribute("data-theme", currentTheme);
+
+	const switchTheme = () => {
+		const currentTheme  = localStorage.getItem("theme") || "light";
+		const nextTheme = currentTheme === "light" ? "dark" : "light";
+		localStorage.setItem("theme", nextTheme);
+		document.documentElement.setAttribute("data-theme", nextTheme);
+	};
+
+	elems.forEach(elem => elem.addEventListener("click", switchTheme));
+}
